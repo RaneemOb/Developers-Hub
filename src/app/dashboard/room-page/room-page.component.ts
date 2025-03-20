76f63f9b6a70490653a35a570cd1ef681cb6a60d
ammarnas <ammarnasser.contact@gmail.com>
2025-03-20 09:58:32 +0300

    integrate ai with backend

--- src/app/dashboard/room-page/room-page.component.ts
+++ src/app/dashboard/room-page/room-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SidebarComponent } from "../shared/sidebar/sidebar.component";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-room-page",
@@ -10,5 +11,26 @@ import { SidebarComponent } from "../shared/sidebar/sidebar.component";
   imports: [CommonModule, SidebarComponent],
 })
 export class RoomPageComponent {
-  // Component logic here
+
+  suggestions: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  suggestProjectForRoom() {
+    // fill the roomId correctly
+    this.getRoomSuggestion(1).subscribe(
+      (response) => {
+        console.log(response);
+        this.suggestions = response;
+      },
+      (error) => {
+        console.error("Error fetching suggestions:", error);
+      }
+    );
+  }
+  private getRoomSuggestion(roomId: number) {
+    return this.http.get<any>(
+      `http://hackathon-ramadan.runasp.net/api/AiIntegration/${roomId}/SuggestRoomProject`
+    );
+  }
 }
